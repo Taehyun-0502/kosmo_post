17757dab1dea2d262e895addaed669eeb7bc10ea
Taehyun-0502 <uiop72323@gmail.com>
2026-04-16 15:36:14 +0900

    init

---
+++ src/main/java/com/naver/sth/KosmoPostApplication.java
@@ -0,0 +1,13 @@
+package com.naver.sth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KosmoPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KosmoPostApplication.class, args);
+	}
+
+}
