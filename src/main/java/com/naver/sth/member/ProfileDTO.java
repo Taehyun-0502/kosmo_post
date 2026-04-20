b3e4219f72bf53a26116a88e1458e06fe2e1ddab
Taehyun-0502 <uiop72323@gmail.com>
2026-04-20 15:16:53 +0900

    member

---
+++ src/main/java/com/naver/sth/member/ProfileDTO.java
@@ -0,0 +1,17 @@
+package com.naver.sth.member;
+
+import com.naver.sth.file.FileDTO;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ProfileDTO extends FileDTO{
+
+	
+	private String username;
+	
+}
